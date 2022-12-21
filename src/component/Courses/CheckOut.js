c7e1e126c4c2789ecdbe2ac97136b6e64bc302fe
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-12-21 23:55:12 +0600

    install typewriter and update home pahe

--- src/component/Courses/CheckOut.js
+++ src/component/Courses/CheckOut.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import toast from "react-hot-toast";
+import Loading from "../Loading/Loading";
 
 const CheckOut = () => {
   const products = useLoaderData();
@@ -13,28 +14,32 @@ const CheckOut = () => {
        toast.success("Congratulations Successfully Checked Out");
   };
 
+  if (!products) {
+    return <Loading></Loading>
+  }
+
   return (
-    <div className="flex justify-center items-center">
-      <div className="py-16 px-4 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container">
+    <div>
+      <div className="py-16 px-4 flex justify-center items-center mx-auto container">
         <div className="flex flex-col justify-start items-start w-full space-y-9">
           <div className="flex justify-start flex-col items-start space-y-2">
-            <p className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800">
+            <p className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 ">
               Checkout
             </p>
           </div>
 
           <div className="flex flex-col xl:flex-row justify-center xl:justify-between space-y-6 xl:space-y-0 xl:space-x-6 w-full">
-            <div className="flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-100 py-7 sm:py-0 xl:py-10 px-10 xl:w-full">
+            <div className="flex flex-col sm:flex-row xl:flex-col justify-center items-center bg-gray-50 py-7 px-10 w-full   ">
               <div className="flex flex-col justify-start items-start w-full space-y-4">
-                <p className="text-xl md:text-2xl leading-normal text-gray-800">
+                <p className="text-xl md:text-2xl leading-normal text-gray-900">
                   {name}
                 </p>
-                <p className="text-base font-semibold leading-none text-gray-600">
+                <p className="text-base font-bold leading-none text-gray-800">
                   {price}
                 </p>
               </div>
-              <div className="mt-6 sm:mt-0 xl:my-10 xl:px-20 w-52 sm:w-96 xl:w-auto">
-                <img src={img} alt="" />
+              <div className="mt-6  ">
+                <img className=" " src={img} alt="" />
               </div>
             </div>
           </div>
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,34 +1,68 @@
-import React from 'react';
+import React from "react";
+import Typewriter from "typewriter-effect";
+import "./Home.css";
 
 const Home = () => {
-
-    return (
+  return (
+    <div>
       <div className="lg:max-w-full relative inset-0 md:max-w-full w-full mx-auto">
-        <div className="relative">
+        <div className="relative mx-auto banner-img ">
           <img
             alt=""
-            src="https://vdc.edu.au/wp-content/uploads/2021/08/210804-Ed-14-Image-2.jpg"
+            src="https://www.nrhosting.com/wp-content/uploads/2021/01/Information-Technology-868x490.jpg"
             className="w-full object-cover h-screen lg:block md:hidden hidden"
           />
           <div className="w-full h-full bg-black opacity-20 absolute top-0 left-0" />
           <img
             alt=""
             src="https://www.nrhosting.com/wp-content/uploads/2021/01/Information-Technology-868x490.jpg"
-            className="lg:hidden w-full h-screen md:block hidden"
+            className="lg:hidden  w-full h-screen md:block hidden"
           />
           <img
             alt=""
-            src="https://www.idgcdn.com.au/article/images/740x500/dimg/dreamstime_m_121137480.jpg"
-            className="lg:hidden w-full h-screen md:hidden block"
+            src="https://www.nrhosting.com/wp-content/uploads/2021/01/Information-Technology-868x490.jpg"
+            className="lg:hidden  w-full h-screen md:hidden block"
           />
-          <div className="absolute lg:top-8 right-0 md:top-3 top-0 lg:px-7 md:px-10 px-4 py-4">
-            <p className="lg:text-6xl md:text-4xl text-3xl font-bold leading-9 text-white">
-              It The Future
-            </p>
+          <div className="absolute lg:top-52 mx-auto lg:right-[390px] md:top-52 md:right-48  top-52 p-3 ">
+            <div className=" flex justify-center items-center flex-col ">
+              <h1 className="lg:text-4xl md:text-3xl text-2xl font-bold leading-9 text-white">
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Artificial Intelligence to",
+                      "write engaging and plagiarism-",
+                      "free articles in seconds",
+                      "Generate content for your ",
+                      "website that will boost ",
+                      "your SEO on autopilot!",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+                {/* Artificial Intelligence to write engaging and plagiarism-free
+                  articles in seconds. Generate content for your website that
+                  will boost your SEO on autopilot */}
+              </h1>
+              <div className="mt-4">
+                <p className="text-md leading-7 text-center text-white">
+                  We Innovate and Integrate Thousands Of Products Using Advanced
+                  Technology.
+                </p>
+              </div>
+              <div className="mt-8 flex justify-center items-center w-full">
+                <button className="hover:bg-blue-600 flex justify-center bg-blue-500 sm:w-32 w-full h-10 py-3">
+                  <p className="text-base font-medium leading-none text-white">
+                    Explore
+                  </p>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Home;
